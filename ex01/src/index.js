bfede740975db323c396e69ff37b3654c503ec6a
Eskodevelop <esmirneretljak@gmail.com>
2021-03-11 01:43:16 +0100

    Day03

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+
+function main() {
+
+    // Only change code below the line
+    console.log('Hello World!');
+    // Only change code below the line
+
+}
+
+main();
+module.expert = main;
---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+
+    // Only change coode below this line
+    // Ja ću bit developer,
+    /*
+    al ne zezam se
+    */
+    // Only change code above this line
+}
+
+main();
+module.export = main;
